0de2585b1433bc027fe04e8850ff12cb862e6302
Pingumask <pingumask@elanta.fr>
2023-04-28 19:12:57 +0200

    init

---
+++ main.js
@@ -0,0 +1,40 @@
+const toggler = document.getElementById('team-toggler');
+
+toggler.addEventListener('click', toggleTeam);
+
+document.addEventListener('keydown', handleKey);
+
+function handleKey(event) {
+	let key = event.key;
+	if (key === ' ') {
+		return toggleTeam();
+	}
+	const token = document.querySelector(`[data-letter="${key}"]`);
+	if (token === null) {
+		return console.log(`Missing letter : ${key}`);
+	}
+	token.classList.toggle('flash');
+	if (token.dataset.height == 3 || token.dataset.height == -3) {
+		return console.log(`Token ${key} already out`);
+	}
+	if (toggler.dataset.team === 'bot') {
+		return moveDown(token);
+	}
+	moveUp(token);
+}
+
+function moveUp(token) {
+	let newHeight = Number(token.dataset.height) - 1;
+	token.dataset.height = newHeight;
+	token.style.transform = `translateY(${newHeight * 7}rem)`;
+}
+
+function moveDown(token) {
+	let newHeight = Number(token.dataset.height) + 1;
+	token.dataset.height = newHeight;
+	token.style.transform = `translateY(${newHeight * 7}rem)`;
+}
+
+function toggleTeam() {
+	toggler.dataset.team = toggler.dataset.team === 'bot' ? 'top' : 'bot';
+}
